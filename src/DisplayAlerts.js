f8abea49daa44fb0a396647e75ef640d59ef69dd
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-08-10 23:11:42 -0500

    added message for no alerts

--- src/DisplayAlerts.js
+++ src/DisplayAlerts.js
@@ -2,23 +2,26 @@
  import AlertCard from './AlertCard'
 
  const DisplayAlerts = ({ alerts }) =>{
-
-     return(
-         <div>
-             {
-                 alerts.map((alert,i)=>{
-                     return <AlertCard
+    if(alerts.length <= 0){
+        return <h1>All Clear! ^.^</h1>
+    } else {
+        return(
+            <div>
+                {
+                    alerts.map((alert,i)=>{
+                        return <AlertCard
                         key={i}
                         location={alert.properties.areaDesc}
                         event={alert.properties.event}
                         expires={alert.properties.expires}
                         headline={alert.properties.headline}
                         instruction={alert.properties.instruction}
-                     />
-                 })
-             }
-         </div>
-     )
+                        />
+                    })
+                }
+            </div>
+        )
+    }
  }
 
 export default DisplayAlerts
